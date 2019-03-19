f4d36c79d59b0a789025efce1c2fa389107c049b
Zhen Dong <devmen@163.com>
2019-03-19 20:32:05 +0800

    update

--- src/main/java/com/uzdz/策略模式/project/Duck.java
+++ src/main/java/com/uzdz/策略模式/project/Duck.java
@@ -16,15 +16,24 @@ public abstract class Duck {
         this.headBehavior = headBehavior;
     }
 
+    /**
+     * 所有的鸭子都会游泳 (不变部分)
+     */
     public void swim() {
         System.out.println("i can swim");
     }
 
+    /**
+     * 鸭子走路的姿势不一样 (变化部分，且无法组合成算法族，只能由子类单独实现，比如备注之类的)
+     */
+    public abstract void walk();
+
+    /**
+     * 鸭子的头不一样 (变化部分，且可以通过实现行为的不同策略，形成算法族)
+     */
     public void head() {
         headBehavior.head();
     }
-
-    public abstract void walk();
 
     /**
      * 策略模式支持运行时更改行为策略
